eb978f68e5ffa17c75af1bcd8f98efee32814197
rhysd <lin90162@yahoo.co.jp>
2025-03-02 00:48:04 +0900

    add timeout config to `deserialize` example

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -1,5 +1,6 @@
 use keybinds::{KeybindDispatcher, Keybinds};
 use serde::Deserialize;
+use std::time::Duration;
 
 // Actions triggered by key bindings
 #[derive(Deserialize, Debug)]
@@ -8,16 +9,27 @@ enum Action {
     ExitApp,
 }
 
+// Keyboard specific configuration
+#[derive(Deserialize)]
+struct KeyboardConfig {
+    // `Keybinds` implements serde's `Deserialize` to deserialize key bindings from a mapping object
+    bindings: Keybinds<Action>,
+    // Timeout on waiting for the next input while the matching is ongoing.
+    timeout: Option<u64>,
+}
+
 // Configuration file format of your application
 #[derive(Deserialize)]
 struct Config {
-    // `Keybinds` implements serde's `Deserialize` to deserialize key bindings from a mapping object
-    bindings: Keybinds<Action>,
+    keyboard: KeyboardConfig,
 }
 
 fn main() {
     let configuration = r#"
-[bindings]
+[keyboard]
+timeout = 500
+
+[keyboard.bindings]
 "Ctrl+Alt+Enter" = "OpenFile"
 "Ctrl+x Ctrl+c" = "ExitApp"
 "#;
@@ -26,7 +38,12 @@ fn main() {
     let config: Config = toml::from_str(configuration).unwrap();
 
     // Use the key bindings parsed from the TOML input
-    let dispatcher = KeybindDispatcher::new(config.bindings);
+    let mut dispatcher = KeybindDispatcher::new(config.keyboard.bindings);
+    // Set the matching timeout if needed
+    if let Some(ms) = config.keyboard.timeout {
+        dispatcher.set_timeout(Duration::from_millis(ms));
+    }
 
     dbg!(dispatcher.keybinds());
+    dbg!(dispatcher.timeout());
 }
